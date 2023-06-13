789e20cfd81bd8b2e66a98d0f0f9578a5f284b46
Naman Agrahari <nagrahari31@gmail.com>
2023-06-13 13:21:20 +0545

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A web based application that allows patients to book appointments, request organs or register for organ and blood donation. Users can search for hospitals based on their needs.
---
+++ public/jsFiles/mail.js
@@ -0,0 +1,106 @@
+require("dotenv").config();
+var nodemailer = require("nodemailer");
+
+var transporter = nodemailer.createTransport({
+  service: "gmail",
+  // host: "smtp.gmail.com",
+  // secure: true,
+  auth: {
+    
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
+
+const sendMail = (text, receiver, cb) => {
+  const mailOptions = {
+    sender: " ",
+    from: "MedNet",
+    to: receiver,
+    subject: "Appointment Started",
+    html:
+      '<p>This is your link to join the meeting <a href="' +
+      text +
+      '">' +
+      text +
+      "</a></p>",
+  };
+
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, data);
+    }
+  });
+};
+const sendReceiptMail = (receiver, fileName, encodedString, cb) => {
+  const mailOptions = {
+    sender: " ",
+    from: "MedNet",
+    to: receiver,
+    subject: "Appointment Receipt",
+    html: "<p>This is your payment Receipt </p>",
+    attachments: [
+      {
+        filename: fileName,
+        content: new Buffer.from(encodedString, "base64"),
+        encoding: "base64",
+        contentType: "application/pdf",
+      },
+    ],
+  };
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, data);
+    }
+  });
+};
+const sendPrescriptionMail = (receiver, fileName, encodedString, cb) => {
+  const mailOptions = {
+    sender: " ",
+    from: "MedNet",
+    to: receiver,
+    subject: "Appointment Prescription",
+    html: "<p>This is your prescription </p>",
+    attachments: [
+      {
+        filename: fileName,
+        content: new Buffer.from(encodedString, "base64"),
+        encoding: "base64",
+        contentType: "application/pdf",
+      },
+    ],
+  };
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, data);
+    }
+  });
+};
+const sendContactUsMail = (message, cb) => {
+  const mailOptions = {
+    sender: " ",
+    from: "MedNet",
+    to: "nuevedoctor@gmail.com",
+    subject: "Help Needed!",
+    html: `<p>Name: ${message.name}</p><p>Email: ${message.email}</p><p>${message.message}</p>`,
+  };
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, data);
+    }
+  });
+};
+module.exports = {
+  sendMail,
+  sendPrescriptionMail,
+  sendContactUsMail,
+  sendReceiptMail
+};
